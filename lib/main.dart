1e4fba5578961f467d9d51915d0ea8bd4b3675bf
Sanambedi <55624366+Sanambedi@users.noreply.github.com>
2021-05-05 01:07:14 +0530

    origin

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,60 @@
 import "package:flutter/material.dart";
-import "./question.dart";
-import "./answer.dart";
+import "./quiz.dart";
+import "./result.dart";
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget{
+class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    
     return _MyAppState();
   }
 }
 
 class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ["Black", "Red", "Green", "White"],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ["Rabbit", "Snake", "Elephant", "Lion"]
+    },
+  ];
   var _questionIndex = 0;
-  void _answerQuestion(){
+  void _answerQuestion() {
     setState(() {
-      if(_questionIndex==1){
-        return;
-      }
       _questionIndex++;
     });
-    print("Question answered");
+    print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print("We have more questions");
+    } else {
+      print("No more questions");
+    }
   }
+
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'questionText':'What\'s your name?',
-        'answers': ["Black","Red","Green","White"],
-      },
-      {
-        'questionText':'What\'s your favorite animal?',
-        'answers': ["Rabbit","Snake","Elephant","Lion"]
-      },      
-    ];
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-      appBar: AppBar(
-        title: Text("My first App"),
-      ),
-      body: Column(
-        children: [
-          Question(
-            questions[_questionIndex]['questionText'],
-          ),
-          ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
-            return Answer(_answerQuestion,answer);
-          }).toList(),
-        ],
-      ),
-    ));
+            appBar: AppBar(
+              title: Text("My first App"),
+            ),
+            body: _questionIndex < _questions.length
+                ? Quiz(
+                    answerQuestion: _answerQuestion,
+                    questionIndex: _questionIndex,
+                    questions: _questions,
+                  )
+                : Result(
+                  
+                )
+      )
+    );
   }
 }
