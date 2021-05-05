73cc9575c93813750dacc43d0fa354bfd26fe28f
Sanambedi <55624366+Sanambedi@users.noreply.github.com>
2021-05-05 16:49:47 +0530

    origin

--- lib/result.dart
+++ lib/result.dart
@@ -2,7 +2,8 @@ import "package:flutter/material.dart";
 
 class Result extends StatelessWidget {
   final int resultScore;
-  Result(this.resultScore);
+  final Function resetHandler;
+  Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase{
     String resultText;
@@ -24,8 +25,20 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(resultPhrase,style:TextStyle(fontSize: 36,fontWeight: FontWeight.bold),
-      textAlign: TextAlign.center,),
+      child: Column(
+        children: <Widget>[
+          Text(resultPhrase,style:TextStyle(fontSize: 36,fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(
+            child: Text("Restart Quiz"),
+            style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.all(Colors.blue[0]),
+            ),
+            onPressed: resetHandler,
+          )
+        ]
+      ),
     );
   }
 }
